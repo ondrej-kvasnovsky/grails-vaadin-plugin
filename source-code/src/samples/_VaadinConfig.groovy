adfa851a1168c0cf8722966a84eebe978a896474
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2012-02-19 11:14:48 +0100

    Added servletClass field to VaadinConfig template

--- source-code/src/samples/_VaadinConfig.groovy
+++ source-code/src/samples/_VaadinConfig.groovy
@@ -1,12 +1,16 @@
 
 vaadin {
 
-    //Your Vaadin application class that extends com.vaadin.Application:
+    // Your Vaadin application class that extends com.vaadin.Application:
     applicationClass = "com.mycompany.MyVaadinApplication"
 
+	// This is optional, GrailsAwareApplicationServlet is provided by default. Use this if you need to add or change application servlet. 
+	// You should extend GrailsAwareApplicationServlet or GrailsAwareGAEApplicationServlet (from com.vaadin.grails.terminal.gwt.server package).
+	// servletClass = "com.mycompany.MyGrailsAwareApplicationServlet"
+	
     autowire = "byName" //how should dependencies be injected? other option is 'byType'
 
-    //the context relative path where you want to access your Vaadin UI.  Default is the context root
+    // The context relative path where you want to access your Vaadin UI. Default is the context root.
     contextRelativePath = "/"
               
     productionMode = false
