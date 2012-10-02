cb10c99a5192509f43a860b2174d981eeb083da3
Ondrej Kvasnovsky <okvasnovsky@vendavo.com>
2012-10-02 22:18:41 +0200

    init & working version of Vaadin 7 plugin

---
+++ grails-vaadin7-plugin/src/groovy/com/vaadin/grails/Grails.groovy
@@ -0,0 +1,119 @@
+package com.vaadin.grails
+
+import org.springframework.beans.BeansException
+import grails.util.Holders
+import org.springframework.context.MessageSource
+import org.springframework.context.ApplicationContext
+import org.springframework.context.i18n.LocaleContextHolder
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Manages access to Grails services (actually beans) and i18n.
+ *
+ * @author Ondrej Kvasnovsky
+ */
+class Grails {
+
+    private static final transient Logger log = LoggerFactory.getLogger(this)
+
+    /**
+     * Returns bean from application context. Example use: Grails.get(UserService).findAllUsers()
+     *
+     * @param clazz bean class
+     * @return bean from the context
+     * @throws BeansException
+     */
+    public static <T> T get(final Class<T> clazz) throws BeansException {
+        ApplicationContext context = getApplicationContext()
+        T res = context.getBean(clazz)
+        return res
+    }
+
+    /**
+     * Returns Grails application context.
+     *
+     * @return grails application context
+     */
+    public static ApplicationContext getApplicationContext() {
+        GrailsApplication application = Holders.getGrailsApplication()
+        ApplicationContext res = application.getMainContext()
+        return res
+    }
+
+    /**
+     * Returns Grails message source from the application context.
+     *
+     * @return grails message source
+     */
+    public static MessageSource getMessageSource() {
+        ApplicationContext context = getApplicationContext()
+        MessageSource res = context.getBean(MessageSource)
+        return res
+    }
+
+    /**
+     * Provides access to i18n values.
+     *
+     * @param key to localization property
+     * @return value from properties file or key (if key value is not found)
+     */
+    public static String i18n(final String key) {
+        Locale locale = LocaleContextHolder.getLocale()
+        String res = i18n(key, locale)
+        return res
+    }
+
+    /**
+     * Provides access to i18n values.
+     *
+     * @param key to localization property
+     * @return value from properties file or key (if key value is not found)
+     */
+    public static String i18n(final String key, final Locale locale) {
+        String res = i18n(key, null, locale)
+        return res
+    }
+
+    /**
+     * Provides access to i18n values.
+     *
+     * @param key to localization property
+     * @param args arguments, e.g. "Hallo {0}"
+     * @param locale locale
+     * @return value from properties file or key (if key value is not found)
+     */
+    public static String i18n(final String key, final Object[] args, final Locale locale) {
+        String res = i18n(key, args, null, locale)
+        return res
+    }
+
+    /**
+     * Provides access to i18n values.
+     *
+     * @param key to localization property
+     * @param args arguments, e.g. "Hello {0}"
+     * @param defaultValue
+     * @param locale locale
+     * @return value from properties file or key (if key value is not found)
+     */
+    public static String i18n(final String key, final Object[] args, final String defaultValue, final Locale locale) {
+        String res = null
+        try {
+            if (defaultValue) {
+                res = getMessageSource().getMessage(key, args, defaultValue, locale)
+            }
+            else {
+                res = getMessageSource().getMessage(key, args, locale)
+            }
+        } catch (final Throwable t) {
+            log.warn(t.getLocalizedMessage())
+        }
+        if (res == null) {
+            // return the key in brackets in case the fetching fails
+            res = "[" + key + "]"
+        }
+        return res
+    }
+}
---
+++ grails-vaadin7-plugin/src/samples/_VaadinConfig.template
@@ -0,0 +1,21 @@
+vaadin {
+
+    // Your Vaadin UI classes that extends com.vaadin.ui.UI.
+    // The application will be available at e.g. http://localhost:8080/grails-vaadin7-demo/
+    mapping = [
+            "/*": "app.MyUI"
+    ]
+
+    // This is optional because the servlet is provided by default.
+    // servletClass = "com.mycompany.MyGrailsAwareApplicationServlet"
+
+    productionMode = false
+}
+
+environments {
+    production {
+        vaadin {
+            productionMode = true
+        }
+    }
+}
