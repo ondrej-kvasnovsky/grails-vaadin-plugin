2de9b71e850aa72901b6929111198475460cb5ae
Ondrej Kvasnovsky <okvasnovsky@vendavo.com>
2012-10-20 20:02:55 +0200

    version number increased (minor fixes for 1.7.0-beta5)

--- grails-vaadin7-plugin/VaadinGrailsPlugin.groovy
+++ grails-vaadin7-plugin/VaadinGrailsPlugin.groovy
@@ -14,7 +14,7 @@ class VaadinGrailsPlugin {
     private static final String DEFAULT_SERVLET = "com.vaadin.grails.GrailsVaadinServlet";
 
     // the plugin version
-    def version = "1.7.0-beta5"
+    def version = "1.7.0-beta5.1"
 
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
