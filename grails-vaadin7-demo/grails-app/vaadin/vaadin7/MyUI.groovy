88664f7e7fbd367aac353999a0662915d6ee6129
Ondrej Kvasnovsky <okvasnovsky@vendavo.com>
2012-11-21 10:36:19 +0100

    demo example for Vaadin 7 fixed (#3 issue closed)

--- grails-vaadin7-demo/grails-app/vaadin/vaadin7/MyUI.groovy
+++ grails-vaadin7-demo/grails-app/vaadin/vaadin7/MyUI.groovy
@@ -6,6 +6,7 @@ import com.vaadin.ui.Label
 import grails.vaadin7.demo.UserService
 import grails.vaadin7.demo.User
 import com.vaadin.grails.Grails
+import com.vaadin.ui.VerticalLayout
 
 /**
  *
@@ -17,10 +18,12 @@ class MyUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         List<User> users = Grails.get(UserService).getListOfUsers()
-        addComponent(new Label(Grails.i18n("aloha")))
-        addComponent(new Label(Grails.i18n("aloha-which-doesnt-exist")))
+        VerticalLayout layout = new VerticalLayout()
+        layout.addComponent(new Label(Grails.i18n("aloha")))
+        layout.addComponent(new Label(Grails.i18n("aloha-which-doesnt-exist")))
         for (User user : users) {
-            addComponent(new Label(user.name))
+            layout.addComponent(new Label(user.name))
         }
+        setContent(layout)
     }
 }
--- grails-vaadin7-demo/grails-app/vaadin/vaadin7/MyUI2.groovy
+++ grails-vaadin7-demo/grails-app/vaadin/vaadin7/MyUI2.groovy
@@ -3,6 +3,7 @@ package vaadin7
 import com.vaadin.ui.UI
 import com.vaadin.server.VaadinRequest
 import com.vaadin.ui.Label
+import com.vaadin.ui.VerticalLayout
 
 /**
  *
@@ -13,6 +14,8 @@ class MyUI2 extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
-        addComponent(new Label("Second awesome UI! God that's smashing... let's roll the joint! :-D"))
+        VerticalLayout layout = new VerticalLayout()
+        layout.addComponent(new Label("Second awesome UI! God that's smashing... let's roll the joint! :-D"))
+        setContent(layout)
     }
 }
