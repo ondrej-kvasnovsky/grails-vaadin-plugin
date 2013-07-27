8cf4a399d143df806b192df129b4c98a0a1b1fd8
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2013-07-27 09:52:04 +0200

    update to 7.1.1

--- grails-vaadin7-plugin/grails-app/conf/BuildConfig.groovy
+++ grails-vaadin7-plugin/grails-app/conf/BuildConfig.groovy
@@ -28,10 +28,10 @@ grails.project.dependency.resolution = {
     }
 
     dependencies {
-        compile 'com.vaadin:vaadin-server:7.1.0'
-        compile 'com.vaadin:vaadin-client-compiled:7.1.0'
-        compile 'com.vaadin:vaadin-client:7.1.0'
-        compile 'com.vaadin:vaadin-themes:7.1.0'
+        compile 'com.vaadin:vaadin-server:7.1.1'
+        compile 'com.vaadin:vaadin-client-compiled:7.1.1'
+        compile 'com.vaadin:vaadin-client:7.1.1'
+        compile 'com.vaadin:vaadin-themes:7.1.1'
     }
 
     plugins {
