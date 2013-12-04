7ae011f5067f74ac30170bd81c9eaac203125df9
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2013-12-04 09:03:53 +0100

    Demo project for Grails 2.3.x and Vaadin 7.1.x

---
+++ grails23-vaadin7-demo/grails-app/conf/VaadinConfig.groovy
@@ -0,0 +1,26 @@
+vaadin {
+
+    // Your Vaadin UI classes that extends com.vaadin.ui.UI.
+    // The application will be available at e.g. http://localhost:8080/grails-vaadin7-demo/
+    mapping = [
+            "/*": "app.MyUI"
+    ]
+
+    // Extra mapping in case you need 'reserve' a URL and it shouldn't be mapped to e.g. /* by Vaadin
+    mappingExtras = [
+            '/console/*'
+    ]
+
+    // This is optional because the servlet is provided by default.
+    // servletClass = "com.mycompany.MyGrailsAwareApplicationServlet"
+
+    productionMode = false
+}
+
+environments {
+    production {
+        vaadin {
+            productionMode = true
+        }
+    }
+}
