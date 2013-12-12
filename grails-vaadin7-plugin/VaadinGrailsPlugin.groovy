56a6828ef686f53fd1c06e3453ae507dcbd08648
Antonio Caiazzo <acaiazzo@byte-code.com>
2013-12-12 17:46:30 +0000

    support for asyc in vaadin servlet

--- grails-vaadin7-plugin/VaadinGrailsPlugin.groovy
+++ grails-vaadin7-plugin/VaadinGrailsPlugin.groovy
@@ -54,6 +54,7 @@ class VaadinGrailsPlugin {
         def applicationServlet = config.servletClass ?: DEFAULT_SERVLET
         def servletName = "VaadinServlet "
         def widgetset = config.widgetset
+        def asyncSupported = config.asyncSupported
 
         def servlets = xml."servlet"
 
@@ -80,6 +81,10 @@ class VaadinGrailsPlugin {
                     }
 
                     "load-on-startup"("1")
+                    
+                    if (asyncSupported) {
+                        "async-supported"("1")
+                    }
                 }
             }
 
