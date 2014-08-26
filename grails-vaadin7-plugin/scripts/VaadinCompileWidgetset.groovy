e8cac1a2b952b668a3c2633290584596aaedf80e
v.zhuravskiy <v.zhuravskiy@bftcom.com>
2014-08-26 11:29:13 +0400

    long classpath problem

--- grails-vaadin7-plugin/scripts/VaadinCompileWidgetset.groovy
+++ grails-vaadin7-plugin/scripts/VaadinCompileWidgetset.groovy
@@ -6,6 +6,7 @@ target(widgetset_init: "init") {
     vaadinConfig = new ConfigSlurper(grails.util.Environment.current.name).parse(new File("${basedir}/grails-app/conf/VaadinConfig.groovy").text)
     ant.property(name: "widgetset", value: vaadinConfig.vaadin.widgetset)
     ant.property(name: "workers", value: vaadinConfig.vaadin.compile.widgetset.workers?:"4")
+    ant.property(name: "forkJava", value: vaadinConfig.vaadin.compile.forkJava != null ? vaadinConfig.vaadin.compile.forkJava : true)
     ant.property(name: "widgetset-path", value: "")
     ant.property(name: "client-side-destination", value: "web-app/VAADIN/widgetsets")
     ant.property(name: "generate.widgetset", value: "1")
@@ -18,8 +19,22 @@ Compiles the widgetset configured in grails-app/conf/VaadinConfig.groovy
 target(compile_widgetset: "Compiles the widgetset configured in grails-app/conf/VaadinConfig.groovy") {
     depends(classpath, compile, widgetset_init)
     ant.echo message: """Compiling ${ant.project.properties.'widgetset'} into ${ant.project.properties."client-side-destination"} directory..."""
-    ant.java(classname: "com.google.gwt.dev.Compiler", maxmemory: "512m", failonerror: true, fork: true, classpathref: "grails.compile.classpath") {
+    ant.property(name: "gcp", refid: "grails.compile.classpath")
+    ant.jar(destfile: "$projectTargetDir/path.jar") {
+        manifest {
+            attribute(name: "Class-Path", value: ant.project.properties.gcp)
+        }
+    }
+    ant.path (id: 'gwt.compile') {
+        pathelement location: "$projectTargetDir/path.jar"
+        def matcher =  (ant.project.properties.gcp =~ "(?ismx)([^;]*(vaadin|gwt-dev|ant)[^;]*);")
+        for (int i = 0; i < matcher.size(); i++) {
+            pathelement location: matcher[i][1]
+        }
+    }
+    ant.java(classname: "com.google.gwt.dev.Compiler", maxmemory: "512m", failonerror: true, fork: ant.project.properties.'forkJava', classpathref: 'gwt.compile') {
         ant.classpath {
+            pathelement location: "$projectTargetDir/path.jar"
             pathelement location: "${basedir}/src/java"
             pathelement location: "${basedir}/target/classes"
         }
