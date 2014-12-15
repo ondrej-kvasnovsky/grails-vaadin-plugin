c5861c5e4fecd720c0280c1d676d24423415cb13
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2014-12-15 13:20:19 -0800

    7.3.6 released.

--- grails-vaadin7-plugin/VaadinGrailsPlugin.groovy
+++ grails-vaadin7-plugin/VaadinGrailsPlugin.groovy
@@ -12,7 +12,7 @@ class VaadinGrailsPlugin {
 //    private static final String DEFAULT_SERVLET = "com.vaadin.grails.GrailsVaadinServlet";
     private static final String DEFAULT_SERVLET = "com.vaadin.grails.server.DefaultServlet"
 
-    def version = "7.3.5"
+    def version = "7.3.6"
     def grailsVersion = "2.0 > *"
     def pluginExcludes = []
 
--- grails-vaadin7-plugin/grails-app/conf/BuildConfig.groovy
+++ grails-vaadin7-plugin/grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
         mavenRepo "http://repo.springsource.org/release"
     }
 
-    vaadinVersion = "7.3.5"
+    vaadinVersion = "7.3.6"
 
     dependencies {
         compile "com.vaadin:vaadin-server:${vaadinVersion}"
