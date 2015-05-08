db7ffbd8b762ffc07955d6c95e376f727206f010
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2015-05-07 22:50:53 -0700

    quick start finished

--- grails3-vaadin7-plugin/src/main/scripts/vaadin-quickstart.groovy
+++ grails3-vaadin7-plugin/src/main/scripts/vaadin-quickstart.groovy
@@ -4,26 +4,34 @@ import java.util.jar.JarFile
 
 description "Creates minimal required files to run Vaadin application in Grails", "grails vaadin-quickstart"
 
+log "Starting to create quick start files..."
 File pluginDir = new File(VaadinGrailsPlugin.class.getProtectionDomain().getCodeSource().getLocation().getPath());
 
-String urlMappings = "META-INF/templates/_UrlMappings.template"
-String vaadinConfig = "META-INF/templates/_VaadinConfig.template"
-String myUi = "META-INF/templates/_MyUI.template"
+String s = File.separator
+String root = "META-INF${s}templates${s}"
+String urlMappings = root + "_UrlMappings.template"
+String vaadinConfig = root + "_VaadinConfig.template"
+String myUi = root + "_MyUI.template"
 
 JarFile jarFile = new JarFile(pluginDir)
 
-String vaadinConfigPath = baseDir.absolutePath + "/grails-app/conf/VaadinConfig.groovy"
+String vaadinConfigPath = baseDir.absolutePath + "${s}grails-app${s}conf${s}VaadinConfig.groovy"
 File vaadinConfigFile = new File(vaadinConfigPath)
 vaadinConfigFile.write(jarFile.getInputStream(jarFile.getJarEntry(vaadinConfig)).text)
+log "Created: " + vaadinConfigPath
 
-String urlMappingsPath = baseDir.absolutePath + "/grails-app/controllers/UrlMappings.groovy"
+String urlMappingsPath = baseDir.absolutePath + "${s}grails-app${s}controllers${s}UrlMappings.groovy"
 File urlMappingsFile = new File(urlMappingsPath)
 urlMappingsFile.write(jarFile.getInputStream(jarFile.getJarEntry(urlMappings)).text)
+log "Created: " + urlMappingsPath
 
-
-String myUiPackage = baseDir.absolutePath + "/src/main/groovy/app"
+String myUiPackage = baseDir.absolutePath + "${s}src${s}main${s}groovy${s}app"
 new File(myUiPackage).mkdir()
+log "Created: " + myUiPackage
 
-String myUiPath = myUiPackage + "/MyUI.groovy"
+String myUiPath = myUiPackage + "${s}MyUI.groovy"
 File myUiFile = new File(myUiPath)
 myUiFile.write(jarFile.getInputStream(jarFile.getJarEntry(myUi)).text)
+log "Created: " + myUiFile.absolutePath
+
+log "Vaadin QuickStart script finished. You can run: grails run-app"
