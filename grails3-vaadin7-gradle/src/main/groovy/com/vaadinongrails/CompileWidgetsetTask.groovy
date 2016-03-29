c1c439118026c898de0c8c220ad150a3de73fca0
Ondrej Kvasnovsky <ondrej.kvasnovsky@gmail.com>
2016-03-28 23:46:33 -0700

    WIP

---
+++ grails3-vaadin7-gradle/src/main/groovy/com/vaadinongrails/CompileWidgetsetTask.groovy
@@ -0,0 +1,192 @@
+package com.vaadinongrails
+
+import grails.util.Environment
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.TaskAction
+
+import static java.io.File.separator
+
+class CompileWidgetsetTask extends DefaultTask {
+
+    CompileWidgetsetTask() {
+
+        dependsOn('classes')
+
+        description = "Compiles Vaadin Addons and components into Javascript."
+
+        project.afterEvaluate {
+            inputs.files(project.configurations.compile)
+
+            project.sourceSets.main.java.srcDirs.each {
+                inputs.files(project.fileTree(it.absolutePath).include('**/*/client/**/*.java'))
+                inputs.files(project.fileTree(it.absolutePath).include('**/*/shared/**/*.java'))
+                inputs.files(project.fileTree(it.absolutePath).include('**/*/public/**/*.*'))
+                inputs.files(project.fileTree(it.absolutePath).include('**/*/*.gwt.xml'))
+            }
+
+            project.sourceSets.main.resources.srcDirs.each {
+                inputs.files(project.fileTree(it.absolutePath).include('**/*/public/**/*.*'))
+                inputs.files(project.fileTree(it.absolutePath).include('**/*/*.gwt.xml'))
+            }
+        }
+    }
+
+    @TaskAction
+    public void run() {
+
+        File file = new File("grails-app${separator}conf${separator}VaadinConfig.groovy")
+        Map vaadinConfig = new ConfigSlurper(Environment.current.name).parse(file.text)
+
+        String widgetSetName = vaadinConfig.vaadin.widgetset
+        if (widgetSetName) {
+            String widgetSetPath = widgetSetName.replaceAll("\\.", separator)
+            File widgetSet = new File("src${separator}main${separator}resources${separator}${widgetSetPath}.gwt.xml")
+            if (widgetSet.exists()) {
+                println "Found: " + widgetSet.absolutePath
+
+                def widgetsetCompileProcess = ['java']
+
+                FileCollection classpath = getClientCompilerClassPath(project)
+
+                Iterable<File> fields = inputs.files
+                fields.forEach {
+                    println "Classpath: " + it
+                }
+
+
+                println "Path to Classpath: " + classpath.asPath
+
+                widgetsetCompileProcess += ['-cp', classpath.asPath]
+
+                widgetsetCompileProcess += 'com.google.gwt.dev.Compiler'
+
+                widgetsetCompileProcess += ['-style', "OBF"]
+                widgetsetCompileProcess += ['-optimize', 0]
+                widgetsetCompileProcess += ['-war', "src/main/webapp/VAADIN/widgetsets"]
+                widgetsetCompileProcess += ['-logLevel', "INFO"]
+                widgetsetCompileProcess += ['-localWorkers', Runtime.getRuntime().availableProcessors() - 1]
+
+                widgetsetCompileProcess += '-draftCompile'
+                widgetsetCompileProcess += '-strict'
+
+                widgetsetCompileProcess += vaadinConfig.vaadin.widgetset
+
+                Process process = widgetsetCompileProcess.execute()
+                boolean failed = false
+                logProcess(project, process, 'widgetset-compile.log', { String output ->
+                    if (output.trim().startsWith('[ERROR]')) {
+                        failed = true
+                    }
+                })
+
+                process.waitFor()
+
+                if (failed) {
+                    throw new GradleException('Widgetset failed to compile. See error log above.')
+                }
+            } else {
+                println "Widgetset file not found: " + widgetSet.absolutePath
+            }
+
+        }
+    }
+
+    static void logProcess(final Project project, final Process process, final String filename, Closure monitor = {}) {
+        if (true) {
+            Thread.start 'Info logger', {
+                try {
+                    def errorOccurred = false
+                    process.inputStream.eachLine { output ->
+                        monitor.call(output)
+                        if (output.contains("[WARN]")) {
+                            project.logger.warn(output.replaceAll("\\[WARN\\]", '').trim())
+                        } else if (output.contains('[ERROR]')) {
+                            errorOccurred = true
+                        } else {
+                            project.logger.info(output.trim())
+                        }
+                        if (errorOccurred) {
+                            // An error has occurred, dump everything to console
+                            project.logger.error(output.replaceAll("\\[ERROR\\]", '').trim())
+                        }
+                    }
+                } catch (IOException e) {
+                    // Stream might be closed
+                }
+            }
+
+            Thread.start 'Error logger', {
+                try {
+                    process.errorStream.eachLine { String output ->
+                        monitor.call(output)
+                        project.logger.error(output.replaceAll("\\[ERROR\\]", '').trim())
+                    }
+                } catch (IOException e) {
+                    // Stream might be closed
+                }
+            }
+        } else {
+            File logDir = new File(project.buildDir, 'logs')
+            logDir.mkdirs()
+
+            final File logFile = new File(logDir, filename)
+            Thread.start 'Info logger', {
+                logFile.withWriterAppend { out ->
+                    try {
+                        def errorOccurred = false
+                        process.inputStream.eachLine { output ->
+                            monitor.call(output)
+                            if (output.contains("[WARN]")) {
+                                out.println "[WARN] " + output.replaceAll("\\[WARN\\]", '').trim()
+                            } else if (output.contains('[ERROR]')) {
+                                errorOccurred = true
+                                out.println "[ERROR] " + output.replaceAll("\\[ERROR\\]", '').trim()
+                            } else {
+                                out.println "[INFO] " + output.trim()
+                            }
+                            out.flush()
+                            if (errorOccurred) {
+                                // An error has occurred, dump everything to console
+                                project.logger.error(output.replaceAll("\\[ERROR\\]", '').trim())
+                            }
+                        }
+                    } catch (IOException e) {
+                        // Stream might be closed
+                    }
+                }
+            }
+            Thread.start 'Error logger', {
+                logFile.withWriterAppend { out ->
+                    try {
+                        process.errorStream.eachLine { output ->
+                            monitor.call(output)
+                            project.logger.error(output.replaceAll("\\[ERROR\\]", '').trim())
+                            out.println "[ERROR] " + output.replaceAll("\\[ERROR\\]", '').trim()
+                            out.flush()
+                        }
+                    } catch (IOException e) {
+                        // Stream might be closed
+                    }
+                }
+            }
+        }
+    }
+
+    static FileCollection getClientCompilerClassPath(Project project) {
+        FileCollection collection = project.sourceSets.main.runtimeClasspath
+        collection += project.sourceSets.main.compileClasspath
+
+        project.sourceSets.main.groovy.srcDirs.each {
+            collection += project.files(it)
+        }
+
+        project.sourceSets.main.java.srcDirs.each { File dir ->
+            collection += project.files(dir)
+        }
+
+        collection
+    }
+}
--- grails3-vaadin7-gradle/src/main/groovy/com/vaadinongrails/VaadinGradlePlugin.groovy
+++ grails3-vaadin7-gradle/src/main/groovy/com/vaadinongrails/VaadinGradlePlugin.groovy
@@ -19,23 +19,57 @@ class VaadinGradlePlugin implements Plugin<Project> {
             Enumeration enumEntries = jar.entries();
             while (enumEntries.hasMoreElements()) {
                 JarEntry file = (JarEntry) enumEntries.nextElement();
-                if (file.name.startsWith("templates/") && !file.name.startsWith("templates/spring/") && !file.isDirectory()) {
+                if (file.name.startsWith("templates${File.separator}") && !file.name.startsWith("templates${File.separator}spring${File.separator}") && !file.isDirectory()) {
                     println file.name
                 }
             }
 
-
             println "File: "
             println pluginJar.absolutePath
 
-            File file = new File("grails-app/conf/VaadinConfig.groovy")
+            File file = new File("grails-app${File.separator}conf${File.separator}VaadinConfig.groovy")
             Map vaadinConfig = new ConfigSlurper(Environment.current.name).parse(file.text)
 
             println vaadinConfig
         }
 
-        project.task('vaadin-compile-widgetset') << {
+        project.task('vaadin-spring-quickstart') << {
+            println "Vaadin QuickStart task"
+//            println project.sourceSets.main
+            File pluginJar = new File(VaadinGradlePlugin.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+            JarFile jar = new JarFile(pluginJar);
+            Enumeration enumEntries = jar.entries();
+            while (enumEntries.hasMoreElements()) {
+                JarEntry file = (JarEntry) enumEntries.nextElement();
+                if (file.name.startsWith("templates${File.separator}") && file.name.startsWith("templates${File.separator}spring${File.separator}") && !file.isDirectory()) {
+                    println file.name
+                }
+            }
 
+            println "File: "
+            println pluginJar.absolutePath
+
+            File file = new File("grails-app${File.separator}conf${File.separator}VaadinConfig.groovy")
+            Map vaadinConfig = new ConfigSlurper(Environment.current.name).parse(file.text)
+
+            println vaadinConfig
         }
+
+        project.tasks.create(name: 'vaadin-compile-widgetset', type: CompileWidgetsetTask, group: 'Vaadin')
     }
+
+//    static FileCollection getCompileClassPathOrJar(Project project) {
+//        def vaadin = project.vaadin as VaadinPluginExtension
+//        FileCollection classpath
+//        if(vaadin.plugin.useClassPathJar) {
+//            // Add dependencies using the classpath jar
+//            BuildClassPathJar pathJarTask = project.getTasksByName(BuildClassPathJar.NAME, true).first()
+//            classpath = project.files(pathJarTask.archivePath)
+//        } else {
+//            classpath = getCompileClassPath(project)
+//        }
+//        classpath
+//    }
+
+
 }
